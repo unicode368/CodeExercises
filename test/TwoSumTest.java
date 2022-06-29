05b09b3b361bac954a3797b15c96cc1cbf72f9d6
unicode <kislaya.sliva@gmail.com>
2022-06-29 19:03:03 +0300

    switching to parametrized tests

--- test/TwoSumTest.java
+++ test/TwoSumTest.java
@@ -1,12 +1,50 @@
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(Parameterized.class)
 public class TwoSumTest {
+    int[] nums;
+    int target;
+    int[] expectedResult;
+    String description;
+
+    public TwoSumTest(int[] nums, int target,
+                         int[] expectedResult, String description) {
+        this.nums = nums;
+        this.target = target;
+        this.expectedResult = expectedResult;
+        this.description = description;
+    }
+
+    @Parameterized.Parameters
+    public static Collection numsAndTargets() {
+        return Arrays.asList(new Object[][] {
+                { new int[]{2,7,11,15} , 9, new int[]{0,1}, "Sum is two first numbers" },
+                { new int[]{3,4,2} , 6, new int[]{1,2}, "Sum is two last numbers" },
+                { new int[]{3,3} , 6, new int[]{0,1}, "Only two numbers of array" },
+                { new int[]{3,3} , 7, null, "No such numbers" },
+                { new int[]{2,4,15,15,2,7,11,15}, 22, new int[]{2,5}, "Numbers in th middle" },
+                { new int[]{2,7,15,15,21,7,1,15}, 22, new int[]{1,2}, "Two occurences" }
+        });
+    }
+
     @Test
-    public void testNine() {
-        int[] nums = new int[]{2,7,11,15};
-        int target = 9;
-        Assertions.assertArrayEquals(new int[]{0,1},
+    public void testAddTwoNumbers() {
+        //System.out.println(description);
+        assertArrayEquals(expectedResult,
+                TwoSum.twoSum(nums, target));
+    }
+    /*public void testNine(int[] nums, int target) {
+        //int[] nums = new int[]{2,7,11,15};
+        //int target = 9;
+        assertArrayEquals(new int[]{0,1},
                 TwoSum.twoSum(nums, target));
     }
 
@@ -14,7 +52,7 @@ public class TwoSumTest {
     public void testSixTwoEdgy() {
         int[] nums = new int[]{3,2,4};
         int target = 6;
-        Assertions.assertArrayEquals(new int[]{1,2},
+        assertArrayEquals(new int[]{1,2},
                 TwoSum.twoSum(nums, target));
     }
 
@@ -22,7 +60,7 @@ public class TwoSumTest {
     public void testSixWithTwoOnly() {
         int[] nums = new int[]{3,3};
         int target = 6;
-        Assertions.assertArrayEquals(new int[]{0,1},
+        assertArrayEquals(new int[]{0,1},
                 TwoSum.twoSum(nums, target));
     }
 
@@ -30,6 +68,6 @@ public class TwoSumTest {
     public void testAbsent() {
         int[] nums = new int[]{3,3};
         int target = 1;
-        Assertions.assertNull(TwoSum.twoSum(nums, target));
-    }
+        assertNull(TwoSum.twoSum(nums, target));
+    }*/
 }
