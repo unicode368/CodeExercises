7b0c8a9f6217b0dfeca70f07e884a906c584b16f
unicode <kislaya.sliva@gmail.com>
2022-06-29 13:56:49 +0300

    two sum with hash map

--- src/TwoSum.java
+++ src/TwoSum.java
@@ -1,11 +1,18 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
 public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         int n = nums.length;
+        HashMap<Integer, Set<Integer>> vals = new HashMap<>();
         for(int i = 0; i < n; i++) {
-            for(int j = i + 1; j < n; j++) {
-                if((nums[i] + nums[j]) == target) {
-                    return new int[]{i, j};
-                }
+            Set<Integer> val = vals.get(target - nums[i]);
+            if (vals.containsKey(target - nums[i])) {
+                return new int[]{val.iterator().next(), i};
+            } else {
+                Set<Integer> set = vals.computeIfAbsent(nums[i], k -> new HashSet<>());
+                set.add(i);
             }
         }
         return null;
