375e9bfe08e499a7e364d6898905bce677e62790
unicode <kislaya.sliva@gmail.com>
2022-07-01 00:01:09 +0300

    more readable solution

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,9 +5,16 @@ public class FizzBuzz {
     public static List<String> fizzBuzz(int n) {
         List<String> list = new ArrayList<>();
         for(int i = 0; i < n; i++) {
-            boolean b = ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) ? list.add("FizzBuzz") :
+            if ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) {
+                list.add("FizzBuzz");
+            } else if ((i + 1) % 5 == 0) {
+                list.add("Buzz");
+            } else if ((i + 1) % 3 == 0) {
+                list.add("Buzz");
+            }
+            /* boolean b = ((i + 1) % 3 == 0 && (i + 1) % 5 == 0) ? list.add("FizzBuzz") :
                     (i + 1) % 3 == 0 ? list.add("Fizz") : (i + 1) % 5 == 0 ?
-                            list.add("Buzz") : list.add(String.valueOf(i + 1));
+                            list.add("Buzz") : list.add(String.valueOf(i + 1));*/
         }
         return list;
     }
