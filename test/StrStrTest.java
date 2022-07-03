76091c67a1e93e9898169698c944590592516727
unicode <kislaya.sliva@gmail.com>
2022-07-03 20:13:37 +0300

    parametrized name

--- test/StrStrTest.java
+++ test/StrStrTest.java
@@ -1,40 +1,33 @@
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@RunWith(Parameterized.class)
 public class StrStrTest {
-    String haystack;
-    String needle;
-    int expectedResult;
-    String description;
 
-    public StrStrTest(String haystack, String needle,
-                      int expectedResult, String description) {
-        this.haystack = haystack;
-        this.needle = needle;
-        this.expectedResult = expectedResult;
-        this.description = description;
+    private static Stream<Arguments> provideParameters() {
+        return Stream.of(
+                Arguments.of("hello", "ll", 2),
+                Arguments.of("aaaaa", "bba", -1),
+                Arguments.of("aaaaa", "", 0),
+                Arguments.of("hello", "ll", 2),
+                Arguments.of("hello", "ll", 2)
+        );
     }
 
-    @Parameterized.Parameters
-    public static Collection numsAndTargets() {
-        return Arrays.asList(new Object[][] {
-                { "hello" , "ll", 2, "Substring is present" },
-                { "aaaaa" , "bba", -1, "Substring is not present" },
-                { "aaaaa" , "", 0, "Substring is empty string"}
-        });
-    }
-
-    @Test
-    public void testAddTwoNumbers() {
-        System.out.println(description);
+    @ParameterizedTest(name = "Index of {1} in {0} is {2}")
+    @MethodSource("provideParameters")
+    public void testStrStr(String haystack, String needle,
+                           int expectedResult) {
         assertEquals(expectedResult,
                 StrStr.strStr(haystack, needle));
     }
