38be4eb75bcf7d3dc6ef7a3de4698765ea54c699
unicode <kislaya.sliva@gmail.com>
2022-07-03 14:10:27 +0300

    value source shouldn't be here

--- test/PowerOfThreeTest.java
+++ test/PowerOfThreeTest.java
@@ -1,19 +1,21 @@
 import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PowerOfThreeTest {
-
-    @Test
-    @ValueSource(ints = {27, 19647, 1, -3}, booleans = {true, false, true, false},
+    @ParameterizedTest
+    @ValueSource(ints = {27, 19647, 1, 0, -3}, booleans = {true, false, true, false, false},
     strings = {"Three to the power of 3", "Aliquot but not power of", "Power of 3 to 0",
-            "Not the power of three"})
+            "Zero", "Not the power of three"})
     public void testIsPowerOfThree(int number, boolean expectedResult,
                                    String description) {
         System.out.println(description);
