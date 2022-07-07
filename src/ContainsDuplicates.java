9a71d92636a99ec15bd839be06bee27800504d38
unicode <kislaya.sliva@gmail.com>
2022-07-07 16:20:41 +0300

    contains duplicates problem solved

---
+++ src/ContainsDuplicates.java
@@ -0,0 +1,13 @@
+import java.util.HashSet;
+
+public class ContainsDuplicates {
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> uniqNumbers = new HashSet<>();
+        for (int num : nums) {
+            if (!uniqNumbers.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
