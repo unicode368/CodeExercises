be37b0886629fc1054e099816e7a8d685748930a
unicode <kislaya.sliva@gmail.com>
2022-07-17 19:01:29 +0100

    amount of fives is always less than 2's

--- src/TrailingZeroes.java
+++ src/TrailingZeroes.java
@@ -1,16 +1,6 @@
 public class TrailingZeroes {
     public static int trailingZeroes(int n) {
-        int twosCounter = 0;
-        int fivesCounter = n / 5;
-        int val;
-        for(int i = n; i > 0; i--) {
-            val = i;
-            while (val % 2 == 0) {
-                twosCounter++;
-                val /= 2;
-            }
-        }
-        return Math.min(fivesCounter, twosCounter);
+        return n / 5;
     }
 
     public static void main(String[] args) {
