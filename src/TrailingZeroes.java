dcaf7367a5b122c0835b3eadb6686e2b4350fc78
unicode <kislaya.sliva@gmail.com>
2022-07-17 19:03:24 +0100

    keeping powers of five in mind

--- src/TrailingZeroes.java
+++ src/TrailingZeroes.java
@@ -1,9 +1,10 @@
 public class TrailingZeroes {
     public static int trailingZeroes(int n) {
-        return n / 5;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(trailingZeroes(30));
+        int res = 0;
+        while (n != 0) {
+            res += n / 5;
+            n /= 5;
+        }
+        return res;
     }
 }
