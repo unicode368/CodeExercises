b2910f0b7f94bd454f2244ca61847ac9a0d3698d
unicode <kislaya.sliva@gmail.com>
2022-07-20 11:49:22 +0100

    stack solution is not ready yet

--- src/MyAtoi.java
+++ src/MyAtoi.java
@@ -1,3 +1,5 @@
+import java.util.Stack;
+
 public class MyAtoi {
     public static int myAtoi(String s) {
         if(s.length() == 0) {
@@ -5,9 +7,8 @@ public class MyAtoi {
         }
         int start = 0;
         int sign = 1;
-        int length;
         long number = 0;
-        char[] digits = new char[200];
+        Stack<Character> digits = new Stack<>();
         while(s.charAt(start) == ' ') {
             ++start;
             if(start == s.length()) {
@@ -20,23 +21,19 @@ public class MyAtoi {
             sign = -1;
             ++start;
         }
-        int i = 0;
         while(start != s.length() &&
                 Character.isDigit(s.charAt(start))) {
-            digits[i] = s.charAt(start);
-            ++i;
+            digits.push(s.charAt(start));
             ++start;
         }
-        length = i;
-        i = 0;
-        while(length > 0) {
-            if(digits[i] != '0' && length > 10) {
-                return sign == -1 ? Integer.MIN_VALUE
-                        : Integer.MAX_VALUE;
+        long powOfTen = 1;
+        while(number < Integer.MAX_VALUE && !digits.empty()) {
+            int digit = Character.getNumericValue(digits.pop());
+            if (digit != 0 && powOfTen >= Integer.MAX_VALUE) {
+                return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
-            number += Math.pow(10, length - 1) * Character.getNumericValue(digits[i]);
-            ++i;
-            --length;
+            number += powOfTen * digit;
+            powOfTen *= 10;
         }
         if (number * sign >= Integer.MAX_VALUE) {
             return Integer.MAX_VALUE;
@@ -44,5 +41,9 @@ public class MyAtoi {
             return Integer.MIN_VALUE;
         }
         return (int) number * sign;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(myAtoi("20000000000000000000"));
     }
 }
