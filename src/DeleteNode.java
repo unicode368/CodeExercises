a17fcd7bf69ae520c5f84b94735b33b8b62f5e1c
unicode <kislaya.sliva@gmail.com>
2022-07-20 10:41:53 +0100

    creating new method for testing

--- src/DeleteNode.java
+++ src/DeleteNode.java
@@ -3,6 +3,9 @@ import listUtils.ListUtils;
 
 public class DeleteNode {
     public static void deleteNode(ListNode node) {
+        if (node == null) {
+            return;
+        }
         ListNode curr = node;
         while(curr.next.next != null) {
             curr.val = curr.next.val;
@@ -10,6 +13,14 @@ public class DeleteNode {
         }
         curr.val = curr.next.val;
         curr.next = null;
+    }
+
+    public static void navigateAndDelete(ListNode head, int val) {
+        ListNode curr = head;
+        while (curr != null && curr.val != val) {
+            curr = curr.next;
+        }
+        deleteNode(curr);
     }
 
 }
