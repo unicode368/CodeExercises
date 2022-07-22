049541b528a7c01cd3f425305899b9bf5da4b96b
unicode <kislaya.sliva@gmail.com>
2022-07-23 00:24:44 +0100

    problem solved into inefficient way

--- src/SpiralMatrix.java
+++ src/SpiralMatrix.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class SpiralMatrix {
@@ -8,35 +9,66 @@ public class SpiralMatrix {
         int row = 0;
         int minRow = 0;
         int minCol = 0;
-        int maxCol = matrix.length;
-        int maxRow = matrix[0].length;
+        int maxCol = matrix.length - 1;
+        int maxRow = matrix[0].length - 1;
         int temp;
         int k = 0;
         while (k < matrix.length * matrix[0].length) {
-            if (row > col || row == col && col == maxCol - 1) {
-                while (row > 0) {
-                    spiralMatrix.add(matrix[col][row]);
-                    k++;
-                    row--;
-                }
-                temp = col;
-                col = row;
-                row = temp;
-            } else {
-                while (col < maxCol) {
-                    spiralMatrix.add(matrix[row][col]);
+            if (row == minRow && col == minCol) {
+                while (col <= maxCol) {
+                    if (k >= matrix.length * matrix[0].length) {
+                        break;
+                    }
+                    spiralMatrix.add(matrix[minRow][col]);
                     k++;
                     col++;
                 }
-                temp = row + 1;
-                row = col - 1;
-                col = temp;
+                col = maxCol;
+                minRow++;
+                row = minRow;
+            } else if(row == maxRow && col == maxCol){
+                while (col >= minCol) {
+                    if (k >= matrix.length * matrix[0].length) {
+                        break;
+                    }
+                    spiralMatrix.add(matrix[maxRow][col]);
+                    k++;
+                    col--;
+                }
+                col = minCol;
+                maxRow--;
+                row = maxRow;
+            } else if(col == maxCol){
+                while (row <= maxRow) {
+                    if (k >= matrix.length * matrix[0].length) {
+                        break;
+                    }
+                    spiralMatrix.add(matrix[row][maxCol]);
+                    k++;
+                    row++;
+                }
+                row = maxRow;
+                maxCol--;
+                col = maxCol;
+            } else {
+                while (row >= minRow) {
+                    if (k >= matrix.length * matrix[0].length) {
+                        break;
+                    }
+                    spiralMatrix.add(matrix[row][col]);
+                    k++;
+                    row--;
+                }
+                row = minRow;
+                minCol++;
+                col = minCol;
             }
         }
         return spiralMatrix;
     }
 
     public static void main(String[] args) {
-        spiralOrder(new int[][]{{1,2}, {3,4}});
+        System.out.println(Arrays.toString(spiralOrder(new int[][]{{1,2,3,4},
+                {5,6,7,8}, {9,10,11,12}, {13,14,15,16}}).toArray()));
     }
 }
