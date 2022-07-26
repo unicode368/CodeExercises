43499d242859cc3846daa2dbf35e46a140d59b74
unicode <kislaya.sliva@gmail.com>
2022-07-26 10:21:59 +0100

    stack solution (not recommended)

--- src/MyAtoi.java
+++ src/MyAtoi.java
@@ -1,7 +1,7 @@
 import java.util.Stack;
 
 public class MyAtoi {
-    public static int myAtoi(String s) {
+    public static int myAtoiStack(String s) {
         if(s.length() == 0) {
             return 0;
         }
@@ -33,7 +33,9 @@ public class MyAtoi {
                 return sign == -1 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
             }
             number += powOfTen * digit;
-            powOfTen *= 10;
+            if (powOfTen <= Integer.MAX_VALUE) {
+                powOfTen *= 10;
+            }
         }
         if (number * sign >= Integer.MAX_VALUE) {
             return Integer.MAX_VALUE;
@@ -42,8 +44,5 @@ public class MyAtoi {
         }
         return (int) number * sign;
     }
-
-    public static void main(String[] args) {
-        System.out.println(myAtoi("20000000000000000000"));
-    }
+    
 }
