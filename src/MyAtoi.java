5db6635a2b63e06f1c307278a2473cf0862ae5ce
unicode <kislaya.sliva@gmail.com>
2022-07-31 20:01:15 +0100

    Update MyAtoi.java

--- src/MyAtoi.java
+++ src/MyAtoi.java
@@ -49,45 +49,32 @@ public class MyAtoi {
         if(s.length() == 0) {
             return 0;
         }
-        int start = 0;
+        int pos = 0;
         int sign = 1;
-        int length;
         long number = 0;
-        char[] digits = new char[200];
-        while(s.charAt(start) == ' ') {
-            ++start;
-            if(start == s.length()) {
-                return (int) number;
-            }
+        while(pos < s.length() && s.charAt(pos) == ' ') {
+            ++pos;
         }
-        if(s.charAt(start) == '+') {
-            ++start;
-        } else if(s.charAt(start) == '-') {
+
+        if (pos == s.length()) {
+            return 0;
+        }
+
+        if(s.charAt(pos) == '+') {
+            ++pos;
+        } else if(s.charAt(pos) == '-') {
             sign = -1;
-            ++start;
+            ++pos;
         }
-        int i = 0;
-        while(start != s.length() &&
-                Character.isDigit(s.charAt(start))) {
-            digits[i] = s.charAt(start);
-            ++i;
-            ++start;
-        }
-        length = i;
-        i = 0;
-        while(length > 0) {
-            if(digits[i] != '0' && length > 10) {
-                return sign == -1 ? Integer.MIN_VALUE
-                        : Integer.MAX_VALUE;
+
+        while(pos != s.length() &&
+                Character.isDigit(s.charAt(pos))) {
+            number = number * 10 + (s.charAt(pos) - '0');
+            if (number >= Integer.MAX_VALUE) {
+                return sign == -1 ? Integer.MIN_VALUE :
+                        Integer.MAX_VALUE;
             }
-            number += Math.pow(10, length - 1) * (digits[i] - '0');
-            ++i;
-            --length;
-        }
-        if (number * sign >= Integer.MAX_VALUE) {
-            return Integer.MAX_VALUE;
-        } else if (number * sign <= Integer.MIN_VALUE) {
-            return Integer.MIN_VALUE;
+            ++pos;
         }
         return (int) number * sign;
     }
